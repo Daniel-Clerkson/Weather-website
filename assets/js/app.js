926c9e547238b8fe757dfba19631867504191a2c
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-01-24 18:09:30 +0100

    c

--- assets/js/app.js
+++ assets/js/app.js
@@ -111,7 +111,7 @@ function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-      console.log("Geolocation is not supported by this browser.");
+      alert("Geolocation is not supported by this browser.");
     }
 }
   
@@ -150,7 +150,7 @@ async function showPosition(position) {
 
     date.innerHTML = `${day}, ${month} ${dayEl}`;
     day1.innerHTML = days[dateObj.getUTCDay()];
-    day2.innerHTML = days[dateObj.getUTCDay() -3];
+    day2.innerHTML = days[dateObj.getUTCDay() -6];
     day3.innerHTML = days[dateObj.getUTCDay() -5];
     day4.innerHTML = days[dateObj.getUTCDay() -4];
     day5.innerHTML = days[dateObj.getUTCDay() -3];
